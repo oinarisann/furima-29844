bfe62a677fc6ce15ef2c8ed897707a7c1cbf7ca3
oinarisann <aldebaran800507@gmail.com>
2020-10-09 19:15:06 +0900

    ログイン時の画面表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, {presence: true, format: { with: /\A\S+@\S+\.\S+\z/ }, uniqueness: { case_sensitive: false }}
+
+  validates :nickname, presence: true, length: { maximum: 40 }
+
+  validates :password, presence: true, format:{ with: /\A[a-zA-Z0-9]+\z/ }, length: { minimum: 6 }
+
+  validates :password_confirmation, presence: true, format:{ with: /\A[a-zA-Z0-9]+\z/ }, length: { minimum: 6 }
+
+  validates :family_name, presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
+
+  validates :family_name_kana, presence: true, 
+  format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
+  validates :last_name_kana, presence: true,
+  format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
+  validates :birth_date, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2020_10_06_080523) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname"
+    t.string "family_name"
+    t.string "last_name"
+    t.string "family_name_kana"
+    t.string "last_name_kana"
+    t.date "birth_date"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
