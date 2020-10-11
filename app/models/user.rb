21682e9f7120f050aa193663228b76d035a4db56
oinarisann <aldebaran800507@gmail.com>
2020-10-11 21:31:28 +0900

    Rubocopを実行後

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
-
   with_options presence: true do
     validates :nickname, length: { maximum: 40 }
 
-    validates :password, format:{ with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]+\z/ }, length: { minimum: 6 }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]+\z/ }, length: { minimum: 6 }
     # /\A(?=.*?[a-z])(?=.*?\d)[\w-]{6,128}+\z/i
 
-    validates :password_confirmation, format:{ with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]+\z/ }, length: { minimum: 6 }
+    validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]+\z/ }, length: { minimum: 6 }
 
-    validates :family_name, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name, format:{ with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
 
-    validates :family_name_kana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
-    validates :last_name_kana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/}
-    validates :birth_date 
+    validates :family_name_kana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ }
+    validates :last_name_kana, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/ }
+    validates :birth_date
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    family_name           {"石井"}
-    last_name             {"善幸"}
-    family_name_kana      {"イシイ"}
-    last_name_kana        {"ヨシユキ"}
-    birth_date            {"1234-12-31"}
-
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    family_name           { '石井' }
+    last_name             { '善幸' }
+    family_name_kana      { 'イシイ' }
+    last_name_kana        { 'ヨシユキ' }
+    birth_date            { '1234-12-31' }
   end
 end
